1bd20f265827e3099621999132c699156085dc64
Osama Alwaly <160242270+OsamaGharibElwaly@users.noreply.github.com>
2025-11-27 19:28:59 +0200

    Revert config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/OsamaGharibElwaly",
-  assetPrefix: "/OsamaGharibElwaly/",
 };
 
 export default nextConfig;
