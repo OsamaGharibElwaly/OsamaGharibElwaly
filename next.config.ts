8673a279a8d6cdeae4b5defd9afe3308187b4bfd
Osama Alwaly <160242270+OsamaGharibElwaly@users.noreply.github.com>
2025-11-27 19:20:04 +0200

    Some changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ده اللي بيخلّي Next يطلع static site في فولدر out
   output: "export",
-
-  // عشان الصور تشتغل كويس مع GitHub Pages من غير Image Optimization سيرفر
   images: {
     unoptimized: true,
   },
-
-  // لو عندك إعدادات تانية هنا سيبها
+  basePath: "/OsamaGharibElwaly",
+  assetPrefix: "/OsamaGharibElwaly/",
 };
 
 export default nextConfig;
