e8a93f143bab61785edbf4c7cc8abf3911d94477
Osama Alwaly <160242270+OsamaGharibElwaly@users.noreply.github.com>
2025-11-27 19:13:35 +0200

    Error Fixed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // ده اللي بيخلّي Next يطلع static site في فولدر out
+  output: "export",
+
+  // عشان الصور تشتغل كويس مع GitHub Pages من غير Image Optimization سيرفر
+  images: {
+    unoptimized: true,
+  },
+
+  // لو عندك إعدادات تانية هنا سيبها
 };
 
 export default nextConfig;
